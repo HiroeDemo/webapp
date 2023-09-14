0fedd9c3c235b4b639cf99c29500d5ad1cc6afbe
HiroeDemo <144421942+HiroeDemo@users.noreply.github.com>
2023-09-14 12:48:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 # タイトル
 st.title('画像の縮小と画質調整')
@@ -16,13 +16,26 @@ if uploaded_file is not None:
     
     # 縮小比率と画質のスライダー
     ratio = st.slider("縮小比率（1/n）", 1, 10, 1)
-    quality = st.slider("画質（1-100）", 1, 100, 100)
+    quality = st.slider("画質（1-100）", 1, 100, 85)
+
+    # 色調整のスライダー
+    brightness = st.slider("明るさ（0.5-3.0）", 0.5, 3.0, 1.0)
+    contrast = st.slider("コントラスト（0.5-3.0）", 0.5, 3.0, 1.0)
+    saturation = st.slider("彩度（0.5-3.0）", 0.5, 3.0, 1.0)
     
     # 画像の縮小
     width, height = image.size
     new_width = int(width / ratio)
     new_height = int(height / ratio)
     resized_image = image.resize((new_width, new_height))
+
+    # 色調整
+    enhancer = ImageEnhance.Brightness(resized_image)
+    resized_image = enhancer.enhance(brightness)
+    enhancer = ImageEnhance.Contrast(resized_image)
+    resized_image = enhancer.enhance(contrast)
+    enhancer = ImageEnhance.Color(resized_image)
+    resized_image = enhancer.enhance(saturation)
     
     # 画質調整と保存
     output_img_path = "resized_image.jpg"
